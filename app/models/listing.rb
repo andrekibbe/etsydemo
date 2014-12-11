5b3c1134a87bde1d7249a16425f3529801af583f
Andre Kibbe <akibbe02@gmail.com>
2014-12-11 11:02:57 -0800

    Move :storage parameter before dropbox credentials

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,8 +5,8 @@ class Listing < ActiveRecord::Base
   else
     has_attached_file :image,
     :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-    :storage => :dropbox,
     :content_type => %w(image/jpeg image/jpg image/png),
+    :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path => ":style/:id_:filename"
     validates_attachment_content_type :image
