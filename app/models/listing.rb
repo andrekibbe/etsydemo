a561ec54be11cbcf821220b5af5c106eea3ade06
Andre Kibbe <akibbe02@gmail.com>
2014-12-11 10:25:26 -0800

    Fix validation in listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,11 +4,11 @@ class Listing < ActiveRecord::Base
     :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
     has_attached_file :image,
-    :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-    validates_attachment_content_type :image,
+    :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :content_type => %w(image/jpeg image/jpg image/png),
     :path => ":style/:id_:filename"
+    validates_attachment_content_type :image
   end
 end
