71a7a1c1cce9862814e60a6dcbbc9b26b1f9d300
Andre Kibbe <akibbe02@gmail.com>
2014-12-12 11:09:49 -0800

    Associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,13 +5,17 @@ class Listing < ActiveRecord::Base
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path => ":style/:id_:filename"
+    validates_attachment_presence :image
     validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
   else
     has_attached_file :image,
     :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  end
 
-  validates :name, :description, :price, presence: true
-  validates :price, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: { greater_than: 0 }
+    validates_attachment_presence :image
+    validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+    
+    belongs_to :user
+  end
 end
